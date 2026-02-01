2696b2dad679dfc31667840ae12c71c94f66b80b
AnaGM2 <anagilmolina2@gmail.com>
2026-02-01 01:31:50 +0100

    Add files via upload

---
+++ Machine-Learning-Extensions/Reinforcement-Learning/RL_GGM/src/__init__.py
@@ -0,0 +1,8 @@
+# Importación de módulos o clases
+from .FrozenLake_custom_rewards import FrozenLakeCustomRewards
+from .TileCoding import TileCodingEnv
+from .MountainCar_custom_rewards import MountainCarCustomRewards
+from .CliffWalking_custom_rewards import CliffWalkingCustomRewards
+
+# Lista de módulos o clases públicas
+__all__ = ['FrozenLakeCustomRewards', 'TileCodingEnv', 'MountainCarCustomRewards', 'CliffWalkingCustomRewards']
